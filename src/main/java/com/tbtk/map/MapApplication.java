43f05980894834dcce8dd8a5ee64ec606aead103
eneskacan <enes.kacan@agu.edu.tr>
2021-04-26 10:27:22 +0300

    Initialize Spring Boot project #3

---
+++ src/main/java/com/tbtk/map/MapApplication.java
@@ -0,0 +1,13 @@
+package com.tbtk.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbtk/map/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbtk.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
