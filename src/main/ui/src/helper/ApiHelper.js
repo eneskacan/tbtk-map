bd8e6e0c1318b48ff82e67e122878019b9126ccb
Yiğit Tekgöz <ygttekgoz@gmail.com>
2021-06-05 01:53:23 +0300

    Update ApiHelper.js

--- src/main/ui/src/helper/ApiHelper.js
+++ src/main/ui/src/helper/ApiHelper.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+﻿import axios from "axios";
 
 /**
  * https://medium.com/swlh/react-api-call-best-practice-c78e14a7e150
@@ -6,8 +6,9 @@ import axios from "axios";
 export default class ApiHelper {
     // Singleton structure
     static myInstance = null;
+
     static getInstance() {
-        if(ApiHelper.myInstance == null) {
+        if (ApiHelper.myInstance == null) {
             ApiHelper.myInstance = new ApiHelper();
         }
         return this.myInstance;
@@ -17,7 +18,47 @@ export default class ApiHelper {
         // this.api_token = null;
         // this.client = null;
         this.api_url = "/api/v1";
-        this.user_id = 1; // TODO: Get current user's id
+        this.user_id = 1;
+    }
+
+    // Save a new user to the database.
+    createUser = (name, pass) => {
+        const body = {
+            name: name,
+            pass: pass
+        }
+
+        return axios(
+            {
+                method: 'post',
+                url: this.api_url + "/users",
+                data: body
+            }
+        );
+    }
+
+    // Confirm user from database.
+    confirmUser = (name, pass) => {
+        const body = {
+            name: name,
+            pass: pass
+        }
+
+        const result = axios(
+            {
+                method: 'post',
+                url: this.api_url + "/login",
+                data: body
+            }
+        );
+
+        result.then(response => {
+            if (response.data.body > 0) {
+                this.user_id = response.data.body
+            }
+        });
+
+        return result;
     }
 
     // Get all locations from the database.
