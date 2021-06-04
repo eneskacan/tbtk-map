2a9a0e8132cdf7911d0562d9c5925b0a808c31c4
Yiğit Tekgöz <ygttekgoz@gmail.com>
2021-06-05 01:16:20 +0300

    Add find methods to user repository

--- src/main/java/com/tbtk/map/repository/UserRepository.java
+++ src/main/java/com/tbtk/map/repository/UserRepository.java
@@ -2,7 +2,13 @@ package com.tbtk.map.repository;
 
 import com.tbtk.map.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.List;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+     List<User> findByNameAndPass(String name, String pass);
+     List<User> findByName(String name);
 }
